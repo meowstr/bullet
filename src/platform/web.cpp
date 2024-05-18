f2cd53eec7f8f50aa27ae44fd9e4abc8294d0492
Meowster <142757105+meowstr@users.noreply.github.com>
2024-05-17 21:42:20 -0400

    add emscripten support

--- src/platform/web.cpp
+++ src/platform/web.cpp
@@ -8,11 +8,44 @@
 
 #include "logging.hpp"
 
+using loop_function_t = void ( * )();
+
 static struct {
     GLFWwindow * window = nullptr;
-    int width = 540;
-    int height = 960;
-} hardware;
+    int width = 800;
+    int height = 800;
+
+    int pending_event_list[ 64 ];
+    int pending_event_count = 0;
+
+    loop_function_t step;
+} intern;
+
+static void push_event( int e )
+{
+    if ( intern.pending_event_count >= 64 ) {
+        ERROR_LOG( "input overflow" );
+    }
+
+    intern.pending_event_list[ intern.pending_event_count ] = e;
+    intern.pending_event_count++;
+}
+
+static void
+handle_mouse_button( GLFWwindow * window, int button, int action, int mods )
+{
+    if ( button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS ) {
+        push_event( EVENT_TOUCH );
+    }
+}
+
+static void
+handle_key( GLFWwindow * window, int key, int scancode, int action, int mods )
+{
+    if ( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS ) {
+        glfwSetWindowShouldClose( window, GLFW_TRUE );
+    }
+}
 
 int hardware_init()
 {
@@ -21,29 +54,29 @@ int hardware_init()
         return 1;
     }
 
-    glfwWindowHint( GLFW_RESIZABLE, 1 );
-    hardware.window = glfwCreateWindow(
-        hardware.width,
-        hardware.height,
-        "meow",
+    glfwWindowHint( GLFW_RESIZABLE, 0 );
+    intern.window = glfwCreateWindow(
+        intern.width,
+        intern.height,
+        "don't",
         nullptr,
         nullptr
     );
 
-    if ( !hardware.window ) {
+    if ( !intern.window ) {
         ERROR_LOG( "failed to create window" );
         return 1;
     }
 
-    glfwMakeContextCurrent( hardware.window );
+    glfwMakeContextCurrent( intern.window );
 
-    // glfwSetWindowSizeCallback( hardware.window, handle_resize );
+    // glfwSetWindowSizeCallback( intern.window, handle_resize );
 
     {
         emscripten_set_canvas_element_size(
             "canvas",
-            int( hardware.width ),
-            int( hardware.height )
+            int( intern.width ),
+            int( intern.height )
         );
         // emscripten_set_resize_callback(
         //     "canvas",
@@ -53,59 +86,75 @@ int hardware_init()
         //);
     }
 
-    glfwSetWindowSize( hardware.window, hardware.width, hardware.height );
-    glViewport( 0, 0, hardware.width, hardware.height );
+    //glfwSetMouseButtonCallback( intern.window, handle_mouse_button );
+    glfwSetKeyCallback( intern.window, handle_key );
+
+    glfwSetWindowSize( intern.window, intern.width, intern.height );
+    glViewport( 0, 0, intern.width, intern.height );
 
     return 0;
 }
 
 void hardware_destroy()
 {
-    glfwDestroyWindow( hardware.window );
+    glfwDestroyWindow( intern.window );
     glfwTerminate();
 }
 
-using loop_function_t = void ( * )();
+
+static void loop() {
+        intern.pending_event_count = 0;
+        glfwPollEvents();
+
+        if ( glfwGetKey( intern.window, GLFW_KEY_J ) == GLFW_PRESS ) {
+            push_event( EVENT_HAMMER_CW );
+        }
+        if ( glfwGetKey( intern.window, GLFW_KEY_K ) == GLFW_PRESS ) {
+            push_event( EVENT_HAMMER_CCW );
+        }
+
+        static int last1 = 0;
+        static int last2 = 0;
+        static int last3 = 0;
+
+        if ( glfwGetKey( intern.window, GLFW_KEY_SPACE ) == GLFW_PRESS ) {
+            if (!last1) {
+            push_event( EVENT_JUMP );
+            }
+            last1 = 1;
+        } else last1 = 0;
+
+        if ( glfwGetKey( intern.window, GLFW_KEY_H ) == GLFW_PRESS ) {
+            if (!last2) {
+            push_event( EVENT_FAST_HAMMER_CW );
+            }
+            last2 = 1;
+        } else last2 = 0;
+
+        if ( glfwGetKey( intern.window, GLFW_KEY_L ) == GLFW_PRESS ) {
+            if (!last3) {
+            push_event( EVENT_FAST_HAMMER_CCW );
+            }
+            last3 = 1;
+        } else last3 = 0;
+
+        intern.step();
+}
 
 void hardware_set_loop( loop_function_t step )
 {
-    emscripten_set_main_loop( step, 0, 1 );
+    intern.step = step;
+    emscripten_set_main_loop( loop, 0, 1 );
 }
 
 int hardware_width()
 {
-    return hardware.width;
+    return intern.width;
 }
 
 int hardware_height()
 {
-    return hardware.height;
-}
-
-float hardware_x_axis()
-{
-    float dx = 0.0f;
-
-    if ( glfwGetKey( hardware.window, GLFW_KEY_A ) == GLFW_PRESS ) {
-        dx -= 1.0f;
-    }
-
-    if ( glfwGetKey( hardware.window, GLFW_KEY_D ) == GLFW_PRESS ) {
-        dx += 1.0f;
-    }
-
-    if ( glfwGetMouseButton( hardware.window, GLFW_MOUSE_BUTTON_LEFT ) ==
-         GLFW_PRESS ) {
-        double x, y;
-        glfwGetCursorPos( hardware.window, &x, &y );
-        if ( x < hardware.width * 0.5f ) {
-            dx = -1.0f;
-        } else {
-            dx = 1.0f;
-        }
-    }
-
-    return dx;
+    return intern.height;
 }
 
 float hardware_time()
@@ -117,23 +166,38 @@ void hardware_rumble()
 {
 }
 
-int hardware_touch_down()
-{
-    return 0;
-}
-
-int hardware_touch_pos_x()
-{
-    return 0;
-}
-
-int hardware_touch_pos_y()
-{
-    return 0;
-}
-
 int * hardware_events( int * out_count )
 {
-    *out_count = 0;
-    return nullptr;
+    *out_count = intern.pending_event_count;
+    return intern.pending_event_list;
+}
+
+float hardware_x_axis()
+{
+    float dx = 0.0f;
+
+    if ( glfwGetKey( intern.window, GLFW_KEY_A ) == GLFW_PRESS ) {
+        dx = -1.0f;
+    }
+
+    if ( glfwGetKey( intern.window, GLFW_KEY_D ) == GLFW_PRESS ) {
+        dx = 1.0f;
+    }
+
+    return dx;
+}
+
+float hardware_y_axis()
+{
+    float dy = 0.0f;
+
+    if ( glfwGetKey( intern.window, GLFW_KEY_W ) == GLFW_PRESS ) {
+        dy = -1.0f;
+    }
+
+    if ( glfwGetKey( intern.window, GLFW_KEY_S ) == GLFW_PRESS ) {
+        dy = 1.0f;
+    }
+
+    return dy;
 }
